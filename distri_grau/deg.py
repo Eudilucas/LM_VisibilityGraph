67edc00c064c44b38e43ac10adfa78da42e44ff4
Eude Lucas <eu.de.luc4@gmail.com>
2023-07-17 07:39:33 -0300

    antes do merge

--- distri_grau/deg.py
+++ distri_grau/deg.py
@@ -9,41 +9,43 @@ def vetor(diretorio):
     y = data[:, 1]
     return x, y
 
-n=input('Qual o tamanho dos arquivos?\n')
-    
+n=input('Digite o tamanho do arquivo: ')
+
+
 n = str(n)
 
 
-diretorio1 = '/home/pc/Desktop/IC/caos/distri_grau/LM_p1/' + n + '.txt'
+diretorio1 = '/home/eude/Desktop/IC/caos/distri_grau/LM_p1/' + n + '.txt'
+
 
 x1, p1 = vetor(diretorio1)
 
 
 x2 = []
 fbp = []
-diretorio2 = '/home/pc/Desktop/IC/caos/distri_grau/LM_fbp/' + n + '.txt'
+diretorio2 = '/home/eude/Desktop/IC/caos/distri_grau/LM_fbp/' + n + '.txt'
 
 x2, fbp =vetor(diretorio2)
 
 x3 = []
 intr = []
-diretorio3 = '/home/pc/Desktop/IC/caos/distri_grau/LM_int/' + n + '.txt'
+diretorio3 = '/home/eude/Desktop/IC/caos/distri_grau/LM_int/' + n + '.txt'
 x3, intr =vetor(diretorio3)
 
 x4 = []
 c1 = []
-diretorio4 = '/home/pc/Desktop/IC/caos/distri_grau/LM_c1/' + n + '.txt'
+diretorio4 = '/home/eude/Desktop/IC/caos/distri_grau/LM_c1/' + n + '.txt'
 x4, c1=vetor(diretorio4)
 
 
 x5 = []
 c2 = []
-diretorio5 = '/home/pc/Desktop/IC/caos/distri_grau/LM_c2/' + n + '.txt'
+diretorio5 = '/home/eude/Desktop/IC/caos/distri_grau/LM_c2/' + n + '.txt'
 x5, c2=vetor(diretorio5)
 
 x6 = []
 fc = []
-diretorio6 = '/home/pc/Desktop/IC/caos/distri_grau/LM_fc/' + n + '.txt'
+diretorio6 = '/home/eude/Desktop/IC/caos/distri_grau/LM_fc/' + n + '.txt'
 x6, fc =vetor(diretorio6)
 
 
@@ -53,23 +55,26 @@ x6, fc =vetor(diretorio6)
 
 
 #fazer o plote de todos os arquivos
-plt.figure(figsize=(5,4),layout='constrained')
-plt.plot(x1,p1,'-o',label ='periodic')
-plt.plot(x2,fbp,'-*',label='feigenbaun point')
-plt.plot(x3,intr,'-x', label='intermitente')
-plt.plot(x4,c1,'-d', label='caos 1')
-plt.plot(x5,c2,'-v', label = 'caos 2')
-plt.plot(x6,fc,'-s',label = 'full caos')
+
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.plot(x1,p1,'-o',label = r"Periodic $\mu= 3.5$", markersize = 0.1)
+ax.plot(x2,fbp,'-*',label= r'Feigenbaum point $\mu= 3.56995$',markersize = 0.1)
+ax.plot(x3,intr,'-x',label= r'Intermittent $\mu= 3.857$',markersize = 0.1)
+ax.plot(x4,c1,'-d',label= r'Chaos 1 $\mu=3.87$',markersize = 0.1)
+ax.plot(x5,c2,'-v',label = r'Chaos 2 $\mu=3.89$',markersize = 0.1)
+ax.plot(x6,fc,'-s',label = r'Full caos $\mu= 4.0$',markersize = 0.1)
 
 
 
 #criar a legenda
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Distribuição de grau')
-plt.grid(True)
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_title('Distribuição de grau')
+ax.grid(True)
 
-plt.legend(title = n+' dados')
+ax.legend(title = n+' dados')
 
 #Mostrar o grafico 
 plt.show()
+
+ax.figure.savefig('/home/eude/Desktop/IC/caos/distri_grau/grafico/'+n+'.png')
